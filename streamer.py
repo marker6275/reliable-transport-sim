a4c9ef070ae7c06f3882487ccd4dcac302345ac2
Mark Li <mkli@Marks-Air.hsd1.il.comcast.net>
2025-02-09 13:32:22 -0600

    part 2

--- streamer.py
+++ streamer.py
@@ -2,32 +2,42 @@
 from lossy_socket import LossyUDP
 # do not import anything else from socket except INADDR_ANY
 from socket import INADDR_ANY
+import struct
 
 
 class Streamer:
-    def __init__(self, dst_ip, dst_port,
-                 src_ip=INADDR_ANY, src_port=0):
-        """Default values listen on all network interfaces, chooses a random source port,
-           and does not introduce any simulated packet loss."""
+    def __init__(self, dst_ip, dst_port, src_ip=INADDR_ANY, src_port=0):
         self.socket = LossyUDP()
         self.socket.bind((src_ip, src_port))
         self.dst_ip = dst_ip
         self.dst_port = dst_port
+        self.next_send_seq = 0
+        self.next_recv_seq = 0
+        self.recv_buffer = {}
 
     def send(self, data_bytes: bytes) -> None:
-        """Note that data_bytes can be larger than one packet."""
-        # Break data into chunks of max 1472 bytes (max UDP payload size)
-        chunk_size = 1472
-        for i in range(0, len(data_bytes), chunk_size):
-            chunk = data_bytes[i:i + chunk_size]
-            self.socket.sendto(chunk, (self.dst_ip, self.dst_port))
+        header_size = 4
+        max_payload = 1472 - header_size
+        for i in range(0, len(data_bytes), max_payload):
+            chunk = data_bytes[i:i + max_payload]
+            header = struct.pack("!I", self.next_send_seq)
+            self.next_send_seq += 1
+            packet = header + chunk
+            self.socket.sendto(packet, (self.dst_ip, self.dst_port))
 
     def recv(self) -> bytes:
-        """Blocks (waits) if no data is ready to be read from the connection."""
-        # this sample code just calls the recvfrom method on the LossySocket
-        data, addr = self.socket.recvfrom()
-        # For now, I'll just pass the full UDP payload to the app
-        return data
+        while True:
+            data, addr = self.socket.recvfrom()
+            seq_num = struct.unpack("!I", data[:4])[0]
+            payload = data[4:]
+            if seq_num not in self.recv_buffer:
+                self.recv_buffer[seq_num] = payload
+            assembled = b""
+            while self.next_recv_seq in self.recv_buffer:
+                assembled += self.recv_buffer.pop(self.next_recv_seq)
+                self.next_recv_seq += 1
+            if assembled:
+                return assembled
 
     def close(self) -> None:
         """Cleans up. It should block (wait) until the Streamer is done with all
